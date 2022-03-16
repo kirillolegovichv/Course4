fe5d3bbd9b66f0d574387b1600ca072cfcb85e22
Kirill <kirill.vasil.oleg@gmail.com>
2022-03-16 10:22:16 +0300

    Add exceptions for each method

--- Task1/Variables.cs
+++ Task1/Variables.cs
@@ -8,21 +8,42 @@ namespace Task1
 {
     static class Variables
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="number1"></param>
+        /// <param name="number2">number2 != 0</param>
+        /// <returns></returns>
         public static double[] GetQuontientAndRemainder(double number1, double number2)
         {
+            if (number2 == 0)
+            {
+                throw new Exception("number2 must be != 0");
+            }
             double result = number1 / number2;
             int ost = Convert.ToInt32(number1) % Convert.ToInt32(number2);
             double [] res = new double [2] {result, ost};
             return res;
         }
 
-        public static double GetResult(double number1, double number2)
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="number1">mustn`t be equal number2</param>
+        /// <param name="number2">mustn`t be equal number1</param>
+        /// <returns></returns>
+        public static double GetResultOfExpression(double number1, double number2)
         {
+            if (number1 == number2)
+            {
+                throw new Exception("number1 mustn`t be equal number2");
+            }
             double result = (5 * number1 + number2 * number2) / (number2 - number1);
             return result;
         }
 
-        public static double[] GetChangeNumbers(double number1, double number2)
+        public static double[] ChangeNumbers(double number1, double number2)
         {
             double c = number1;
             number1 = number2;
@@ -31,14 +52,39 @@ namespace Task1
             return result;
         }
 
-        public static double GetX(double a, double b, double c)
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a">mustn`t be equal 0</param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public static double GetXOfLinearEquation(double a, double b, double c)
         {
+            if (a == 0)
+            {
+                throw new Exception("'a' mustn`t be equal 0");
+            }
             double x = (c - b) / a;
             return x;
         }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x1">mustn`t be equal x2</param>
+        /// <param name="y1">mustn`t be equal x1</param>
+        /// <param name="x2"></param>
+        /// <param name="y2"></param>
+        /// <returns></returns>
         public static string GetEquation(double x1, double y1, double x2, double y2)
         {
+            if (x1 == x2)
+            {
+                throw new Exception("x1 mustn`t be equal x2");
+            }
             double a = -(y1 - y2);
             double b = -(x1 * y2 - x2 * y1);
             double c = x2 - x1;
